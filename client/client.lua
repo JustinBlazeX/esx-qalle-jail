0556550581065dac8167ed868c0349435b4e5948
Justin <88727413+JustinBlazeX@users.noreply.github.com>
2024-05-05 14:39:06 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -18,7 +18,7 @@ local jailTime = 0
 
 Citizen.CreateThread(function()
 	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		ESX = exports["es_extended"]:getSharedObject()
 		Citizen.Wait(0)
 	end
 
